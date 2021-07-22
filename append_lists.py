2c2c39e2ed4715047005d8f714ccb9b15e8129f2
Karuna7 <karunkaruna72@gmail.com>
2021-07-22 03:14:18 -0700

    Added if and else statement

--- append_lists.py
+++ append_lists.py
@@ -1,3 +1,4 @@
+#This is to find  something  crazy.
 def list_append(listA, listB):
 	C = listA + listB
 	return C
@@ -5,3 +6,10 @@ def list_append(listA, listB):
 A = [1, 4, 6, 9]
 B = [2, 5, 7]
 print(list_append(A, B))
+
+
+if 2 in a:
+	print("True")
+else:
+	print("False")
+
